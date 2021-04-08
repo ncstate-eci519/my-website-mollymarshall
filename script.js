2996664b1fb7db8e05f0405b4bb7395b9976ea87
mollymarshall <77705042+mollymarshall@users.noreply.github.com>
2021-04-08 19:33:08 -0400

    cat api

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-    $('#date').datepicker({
-      format: 'mm/dd/yyyy',
-      startDate: '-3d'
-    });
+  $('#date').datepicker({
+    format: 'mm/dd/yyyy',
+    startDate: '-3d'
+  });
+
+    $("#cat-button").click(function(){
+        //Execute the API call.
+        var cat = $.get("https://api.thecatapi.com/v1/images/search");
+        console.log(cat);
+        //Function that runs when the API runs successfully
+        cat.done(function(response){
+            
+          //Because the API returns an array, get the first element.
+          var catData = response[0];
+          //Show the cat block
+          $("#cat").show();
+          //Set the SRC of the image to the 
+          $("#cat-img").attr("src", catData.url);
+        });
+      });
 
     $("#phone").change(function(){
         var number = $("#phone").val();
